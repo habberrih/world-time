d9bb59e588dde108bb4b1b80aca0cab0b477bdbe
habberrih <abdul2013ly@gmail.com>
2023-11-25 16:51:19 +0200

    style: change the main class name into MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,23 @@
-import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
 
 void main() {
   runApp(
     DevicePreview(
       enabled: true,
-      builder: (context) => const MaterialApp(home: Home()),
+      builder: (BuildContext context) => const MyApp(),
     ),
   );
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('World Time'),
-        centerTitle: true,
-        backgroundColor: Colors.red[300],
-      ),
-      body: Center(
-        child: Text(
-          'Hello Abdullah!',
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600]),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.red[300],
-        child: const Text('click'),
-      ),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
