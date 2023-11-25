c4bbc92e9e13aa9f5e31eabf3436a1f2f9a9c790
habberrih <abdul2013ly@gmail.com>
2023-11-25 20:24:34 +0200

    refactor: fetch data from world time api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/date_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +13,30 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getTime() async {
+    const String uri = 'https://worldtimeapi.org/api/timezone/Africa/Tripoli';
+
+    http.Response res = await http.get(Uri.parse(uri));
+    Map data = jsonDecode(res.body);
+
+    final dataObject = LocalDate(
+      dateTime: data['datetime'],
+      offset: data['utc_offset'].substring(1, 3),
+      timeZone: data['timezone'],
+    );
+
+    DateTime now = dataObject.setTimeDate(dataObject.dateTime);
+
+    now = now.add(Duration(hours: int.parse(dataObject.offset)));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
