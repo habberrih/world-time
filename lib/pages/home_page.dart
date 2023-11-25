b048936960c907294a08c0d30a90f75e044bf707
habberrih <abdul2013ly@gmail.com>
2023-11-25 23:35:48 +0400

    refactor: receive data from other screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,8 +8,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Map dataFromWorldTimeClass = {};
   @override
   Widget build(BuildContext context) {
+    
+    //! fix: error;
+    dataFromWorldTimeClass = ModalRoute.of(context)!.settings.arguments as Map;
+    print(dataFromWorldTimeClass);
+    //WorldTime dataFromWorldTimeClass = WorldTime(location: data['location'], flag: data['flag'], url: data['url']);
+    //print(dataFromWorldTimeClass);
     return Scaffold(
       body: Column(
         children: <Widget>[
@@ -19,7 +26,13 @@ class _HomePageState extends State<HomePage> {
             },
             icon: const Icon(Icons.edit_location),
             label: const Text('Edit location'),
-          )
+          ),
+          const SizedBox(height: 20.0),
+          /*Row(
+            children: <Widget> [
+              Text(dataFromWorldTimeClass.time!),
+            ],
+          )*/
         ],
       ),
     );
