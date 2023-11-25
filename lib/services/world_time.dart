f778b5b7ee538bab4d832562b5aec2e0950eebe2
habberrih <abdul2013ly@gmail.com>
2023-11-25 23:34:10 +0400

    feat: add WorldTime class

---
+++ lib/services/world_time.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+import '../models/date_time.dart';
+
+
+class WorldTime {
+  String location;
+  String? time;
+  String flag;
+  String url;
+
+  WorldTime({
+    required this.location,
+    required this.flag,
+    required this.url,
+  });
+
+  Future<void> getTime() async {
+    try {
+      String uri = 'https://worldtimeapi.org/api/timezone/$url';
+
+      http.Response res = await http.get(Uri.parse(uri));
+      Map data = jsonDecode(res.body);
+
+      final dataObject = LocalDate(
+        dateTime: data['datetime'],
+        offset: data['utc_offset'].substring(1, 3), //* takes only the hours
+        timeZone: data['timezone'],
+      );
+
+      DateTime now = dataObject.setTimeDate(dataObject.dateTime);
+
+      now = now.add(Duration(hours: int.parse(dataObject.offset)));
+      time = DateFormat.jm().format(now);
+
+    } catch (e) {
+      time = 'Could not get time';
+    }
+  }
+  
+}
