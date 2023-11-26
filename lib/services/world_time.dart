c26b0d9dd0a97c5c82ce83647b01fa2e9ad545f0
habberrih <abdul2013ly@gmail.com>
2023-11-26 12:44:51 +0200

    refactor: add isDayTime parameter

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -5,12 +5,12 @@ import 'package:intl/intl.dart';
 
 import '../models/date_time.dart';
 
-
 class WorldTime {
   String location;
   String? time;
   String flag;
   String url;
+  bool? isDayTime;
 
   WorldTime({
     required this.location,
@@ -28,17 +28,16 @@ class WorldTime {
       final dataObject = LocalDate(
         dateTime: data['datetime'],
         offset: data['utc_offset'].substring(1, 3), //* takes only the hours
-        timeZone: data['timezone'],
+        //timeZone: data['timezone'],
       );
 
       DateTime now = dataObject.setTimeDate(dataObject.dateTime);
 
       now = now.add(Duration(hours: int.parse(dataObject.offset)));
+      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
       time = DateFormat.jm().format(now);
-
     } catch (e) {
       time = 'Could not get time';
     }
   }
-  
 }
